56842e126814603b8d9e0684946c6e133039f4a0
jessicabui <jessicabui97@gmail.com>
2019-02-27 11:49:30 -0500

    Hero Update()

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Heri : MonoBehaviour {
+public class Hero : MonoBehaviour {
+    static public Hero S;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    public float shieldLevel = 1;
+
+    public bool __________________________;
+
+    void Awake()
+    {
+        S = this;
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
 	}
 }
